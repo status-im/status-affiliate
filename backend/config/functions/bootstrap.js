2a042320d9647898858f9294bf6a23efb4cb5524
Jakub Sokołowski <jakub@status.im>
2020-11-09 22:16:45 +0100

    add bootstrapping of admin user in bootstrap.js
    this doesn't work the first time
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -10,4 +10,38 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+async function bootstrap_admin() {
+  const params = {
+    username:   process.env.ADMIN_USER,
+    password:   process.env.ADMIN_PASS,
+    firstname:  process.env.ADMIN_USER,
+    lastname:   process.env.ADMIN_USER,
+    email:      process.env.ADMIN_EMAIL,
+    blocked:    false,
+    isActive:   true,
+  }
+
+  const admins = await strapi.query('user', 'admin').find({ _limit: 1 })
+  if (admins.length) {
+    console.error(`Admin user already exists: ${admins[0].email}`)
+    return
+  }
+
+  try {
+    // in the admin under services you can find the super admin role
+    const superAdminRole = await strapi.admin.services.role.getSuperAdmin()
+    // Hash password before storing in the database
+    console.dir(superAdminRole)
+    params.roles = [superAdminRole.id]
+    params.password = await strapi.admin.services.auth.hashPassword(params.password)
+    // Create admin account
+    const admin = await strapi.query('user', 'admin').create({...params})
+    console.info('Admin account created:', admin)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = async () => {
+  await bootstrap_admin()
+}
